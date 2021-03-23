e9fdce1432b332611b09facf0e2bd7aa3db2c5fd
FuentesRichard <richardfuentesrufino@gmail.com>
2021-03-22 21:50:21 -0300

    Projeto 2

---
+++ Primeiro/Projeto2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double largura, comprimento, preçoMQ, area, preço;
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            preçoMQ = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            preço = area * preçoMQ;
+
+            Console.WriteLine("AREA = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PRECO = " + preço.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
