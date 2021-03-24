6cfab0b5db5c3097d82bd671c9d41720bf816368
FuentesRichard <richardfuentesrufino@gmail.com>
2021-03-24 19:50:52 -0300

    Projeto9

---
+++ Projeto9/Projeto9/Projeto9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double N1, N2, NF;
+
+            N1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            N2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            NF = double.Parse((N1 + N2).ToString("F2"), CultureInfo.InvariantCulture); 
+
+            Console.WriteLine("NOTA FINAL = " + NF.ToString("F2"), CultureInfo.InvariantCulture);
+            if (NF < 60.00)
+            {
+
+                Console.WriteLine("REPROVADO");
+
+            }
+
+
+
+        }
+    }
+}
