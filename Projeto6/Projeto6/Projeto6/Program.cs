f3a3bd21bb626fb8ed1d032e5765364406c764eb
FuentesRichard <richardfuentesrufino@gmail.com>
2021-03-24 14:30:15 -0300

    Projeto6

---
+++ Projeto6/Projeto6/Projeto6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double R, pi, Volume;
+            pi = 3.14159;
+
+            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Volume = (double)(4 / 3.0) * pi * Math.Pow(R, 3);
+
+            Console.WriteLine("VOLUME = " + Volume.ToString("F3"), CultureInfo.InvariantCulture);
+
+        }
+    }
+}
