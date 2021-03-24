6679daed8f635e22827d434d4d757ddcb8a45253
FuentesRichard <richardfuentesrufino@gmail.com>
2021-03-24 14:21:37 -0300

    Projeto5

---
+++ Projeto5/Projeto5/Projeto5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String Nome;
+            double SalarioFixo, Montante, Total;
+
+            Nome = Console.ReadLine();
+            SalarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Montante = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Total = (double)SalarioFixo + 15 * Montante / 100;
+            
+            Console.WriteLine("TOTAL = R$ " + Total.ToString("F2"), CultureInfo.InvariantCulture);
+
+
+        }
+    }
+}
