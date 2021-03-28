87e3b3057d05e034cddd46fd86c295a33a6a7807
FuentesRichard <richardfuentesrufino@gmail.com>
2021-03-28 14:30:22 -0300

    Projeto 10

---
+++ Projeto10/Projeto10/Projeto10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Que horas sao?");
+            x = int.Parse(Console.ReadLine());
+
+            if (x < 12) { 
+            
+                Console.WriteLine("Bom dia!");
+            }
+            
+            else {
+
+                if (x < 18)
+                {
+                    Console.WriteLine("Boa tarde!");
+                }
+                else
+                {
+                    Console.WriteLine("Boa noite!");
+                }
+            }
+
+            Console.ReadLine();
+         
+
+        }
+    }
+}
