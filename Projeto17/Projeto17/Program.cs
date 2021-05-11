0a43cd26d933ae7c62bd620a21ae2fa0adca3d94
FuentesRichard <richardfuentesrufino@gmail.com>
2021-05-11 16:58:24 -0300

    Projeto17

---
+++ Projeto17/Projeto17/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Projeto17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int I, F, R;
+            String[] Horas = Console.ReadLine().Split(' ');
+            I = int.Parse(Horas[0]);
+            F = int.Parse(Horas[1]);
+
+            if (F > I)
+            {
+                R = F - I;
+                Console.WriteLine("O JOGO DUROU " + R + " HORA(S)");
+            }
+            else if (I > F)
+            {
+                R = 24 - (I - F);
+                Console.WriteLine("O JOGO DUROU " + R + " HORA(S)");
+            }
+            else if (I == F)
+            {
+                R = 24;
+                Console.WriteLine("O JOGO DUROU " + R + " HORA(S)");
+            }
+        }
+    }
+}
