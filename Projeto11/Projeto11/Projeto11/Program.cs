fb60675fae7b3198bdfebcd4951de6767afaa64c
FuentesRichard <richardfuentesrufino@gmail.com>
2021-05-11 04:01:46 -0300

    Projeto12

--- Projeto11/Projeto11/Projeto11/Program.cs
+++ Projeto11/Projeto11/Projeto11/Program.cs
@@ -7,32 +7,26 @@ namespace Projeto11
     {
         static void Main(string[] args)
         {
-            double A, B, C, x1, x2, Delta;
 
-            string[] vet;
-            vet = Console.ReadLine().Split(" ");
-         
-            A = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            double A, B, C, X1, X2, Delta;
+            string[] vet = Console.ReadLine().Split(' ');
+            A = double.Parse(vet [0], CultureInfo.InvariantCulture);
             B = double.Parse(vet[1], CultureInfo.InvariantCulture);
             C = double.Parse(vet[2], CultureInfo.InvariantCulture);
-            Delta = Math.Pow(B, 2.0) - 4 * A * C;
-            x1 = (-(B) + Math.Sqrt(Delta)) / (2.0 * A);
-            x2 = (-(B) - Math.Sqrt(Delta)) / (2.0 * A);
-
-            if (A == 0 || Delta < 0.0) {
-
-                Console.WriteLine("Impossivel Calcular");
+            Delta = Math.Pow(B, 2.0) - (4 * A * C);
+            X1 = (-(B) + Math.Sqrt(Delta)) / (2.0 * A);
+            X2 = (-(B) - Math.Sqrt(Delta)) / (2.0 * A);
             
+
+            if (Delta < 0 || A == 0)
+            {
+                Console.WriteLine("Impossivel calcular");
             }
-
-            else {
-
-                Console.WriteLine("R1 = " + x1.ToString("F5", CultureInfo.InvariantCulture));
-                Console.WriteLine("R2 = " + x2.ToString("F5", CultureInfo.InvariantCulture));
-
+            else
+            {
+                Console.WriteLine("R1 = " + X1.ToString("F5",CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + X2.ToString("F5",CultureInfo.InvariantCulture));
             }
-
-
 
         }
     }
