5ce054b1a3965ab52e416f95d630015830df9f61
FuentesRichard <richardfuentesrufino@gmail.com>
2021-05-14 13:56:28 -0300

    Projeto25

---
+++ Projeto24/Projeto24/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Projeto24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, media;
+            nota1 = double.Parse(Console.ReadLine());
+            nota2 = double.Parse(Console.ReadLine());
+
+            while (nota1 < 0 || nota1 > 10)
+            {
+                Console.WriteLine("nota invalida");
+                nota1 = double.Parse(Console.ReadLine());
+            }
+            while (nota2 < 0 || nota2 > 10)
+            {
+                Console.WriteLine("nota invalida");
+                nota2 = double.Parse(Console.ReadLine());
+            }
+            media = (nota1 + nota2) / 2;
+            Console.WriteLine("media = " + media);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Projeto25/Projeto25/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Projeto25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x, alcool, Gasolina, Diesel;
+            x = int.Parse(Console.ReadLine());
+            alcool = 0;
+            Gasolina = 0;
+            Diesel = 0;
+
+            while (x != 4)
+            {
+                while (x < 1 || x > 4)
+                {
+                    x = int.Parse(Console.ReadLine());
+                }
+                while (x == 1)
+                {
+                    alcool = alcool + 1;
+                    x = int.Parse(Console.ReadLine());
+                }
+                while (x == 2)
+                {
+                    Gasolina = Gasolina + 1;
+                    x = int.Parse(Console.ReadLine());
+                }
+                while (x == 3)
+                {
+                    Diesel = Diesel + 1;
+                    x = int.Parse(Console.ReadLine());
+                }
+            }
+            {
+                Console.WriteLine("MUITO OBRIGADO");
+                Console.WriteLine("Alcool: " + alcool);
+                Console.WriteLine("Gasolina: " + Gasolina);
+                Console.WriteLine("Diesel: " + Diesel);
+            }
+        }
+    }
+}
