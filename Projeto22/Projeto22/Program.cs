3d2184e34f1e784b0c5f6fa4b0bdd10548fe9124
FuentesRichard <richardfuentesrufino@gmail.com>
2021-05-14 12:02:05 -0300

    Projeto 22

---
+++ Projeto22/Projeto22/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Projeto22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int pass;
+            pass = int.Parse(Console.ReadLine());
+            while (pass != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                pass = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido");
+
+        }
+    }
+}
