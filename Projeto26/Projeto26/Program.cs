83ccc0638c2eb8c15bd0a194e72eb06c739eb979
FuentesRichard <richardfuentesrufino@gmail.com>
2021-05-14 14:09:38 -0300

    Projeto26

---
+++ Projeto26/Projeto26/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double i, soma, quant, media;
+            i = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            soma = 0;
+            quant = 0;
+            
+            while (i > 0)
+            {
+                soma = soma + i;
+                quant = quant + 1;
+                i = double.Parse(Console.ReadLine());
+            }
+
+            media = soma / quant;
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
