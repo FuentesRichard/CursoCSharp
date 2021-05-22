c3cb064c398c21826b1bcf777b56fbfe013d7f84
FuentesRichard <richardfuentesrufino@gmail.com>
2021-05-22 15:30:06 -0300

    Projeto29

---
+++ Projeto29/Projeto29/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+
+            int X;
+
+            X = int.Parse(Console.ReadLine());
+
+            if (X >= 1 || X <= 1000)
+            {
+                for (int i = 1; i <= X; i=i+2)
+                {                   
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
