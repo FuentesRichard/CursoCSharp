b7e126a8335f52f9e7ffa4a284f95c7894cb5c4b
FuentesRichard <richardfuentesrufino@gmail.com>
2021-05-21 21:48:17 -0300

    Projeto28

---
+++ Projeto27/Projeto27/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Projeto27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = int.Parse(Console.ReadLine());
+
+            while (x != 0)
+            {
+                if (x % 2 == 0)
+                {
+                    Console.WriteLine(5 * x + 20);
+                    x = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine(x = 5 * x + 25);
+                    x = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
---
+++ Projeto28/Projeto28/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Projeto28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N, i;
+            N = int.Parse(Console.ReadLine());
+            if (N > 2 || N < 1000)
+            {
+                for (i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(N + " x " + i + " = " + (N * i));
+                }
+            }
+        }
+    }
+}
