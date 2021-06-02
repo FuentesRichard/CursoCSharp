65e85917b52883530b55dc4bf7b60845500f38db
FuentesRichard <richardfuentesrufino@gmail.com>
2021-06-02 18:33:08 -0300

    Projeto38

---
+++ Projeto38/Projeto38/Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto38
+{
+    class Funcionario
+    {
+       
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public double AumentarSalario(double porcentagem)
+        {
+            porcentagem = porcentagem / 100.00;
+            SalarioBruto = SalarioBruto * porcentagem + SalarioBruto;
+            return SalarioBruto;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", "
+                + "$"
+                + SalarioBruto.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Projeto38/Projeto38/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Funcionario F = new Funcionario();
+
+            Console.Write("Nome: ");
+            F.Nome = Console.ReadLine();
+            Console.Write("Salario bruto: ");
+            F.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            F.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionario: " + F);
+
+            Console.Write("Digite a porcentagem para aumentar o salario: ");
+            double p = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            F.AumentarSalario(p).ToString("F2", CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados atualizados: " + F);
+        }
+    }
+}
