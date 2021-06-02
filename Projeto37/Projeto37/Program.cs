a2b1985cf5b9ebec4b69ed359dc11133bf159d2e
FuentesRichard <richardfuentesrufino@gmail.com>
2021-06-02 03:53:07 -0300

    Projeto37

---
+++ Projeto37/Projeto37/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo R = new Retangulo();
+            
+            Console.WriteLine("Entre a largura e altura do retangulo:");
+            R.Largura = double.Parse(Console.ReadLine());
+            R.Altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("AREA = " + R.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + R.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + R.Diagonal().ToString("F2",CultureInfo.InvariantCulture));
+            
+        }
+    }
+}
---
+++ Projeto37/Projeto37/Retangulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto37
+{
+    class Retangulo
+    {
+
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return (Largura + Altura) * 2;
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+        }
+
+    }
+}
