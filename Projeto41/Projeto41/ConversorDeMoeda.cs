c01ff59fa1eff4edbf6f935059e1fd4ed1c9f420
FuentesRichard <richardfuentesrufino@gmail.com>
2021-06-06 23:43:24 -0300

    Projeto41

---
+++ Projeto41/Projeto41/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto41
+{
+    class ConversorDeMoeda
+    {
+        public static double Dolar;
+
+        public static double ValorEmReais(double d)
+        {
+            return d * Dolar * 106.00 / 100.00;
+        }
+
+    }
+}
---
+++ Projeto41/Projeto41/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Qual e a cotaçao do dolar? ");
+            ConversorDeMoeda.Dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dolares voce vai comprar? ");
+            double dolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double reais = ConversorDeMoeda.ValorEmReais(dolares);
+            Console.WriteLine("Valor a ser pago em reais = " + reais.ToString("F2", CultureInfo.InvariantCulture));
+        
+        }
+    }
+}
