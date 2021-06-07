0312b7e2744221aaaec27e643d77c6470461c0a6
FuentesRichard <richardfuentesrufino@gmail.com>
2021-06-06 23:13:37 -0300

    Projeto40

---
+++ Projeto40/Projeto40/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto40
+{
+    class Calculadora
+    {
+
+        public static double Pi = 3.14;
+
+        public static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+        public static double Volume(double r)
+        {
+            return 4.0 * Pi * Math.Pow(r, 3) / 3.0;
+        }
+    }
+}
