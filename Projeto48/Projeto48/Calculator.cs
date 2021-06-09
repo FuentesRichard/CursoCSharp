4817e503401660839bf059237c0f6d5072e52c12
FuentesRichard <richardfuentesrufino@gmail.com>
2021-06-09 18:14:53 -0300

    Projeto48/ModificadorDeParametroOut

---
+++ Projeto48/Projeto48/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Projeto48
+{
+    class Calculator
+    {
+        public static void Triple(int origin, out int result)
+            //Passar um valor de origem(int origin) e guardar o resultado em outro parametrod e saida(out int result)
+        {
+            result = origin * 3;
+        }
+    }
+}
---
+++ Projeto48/Projeto48/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace Projeto48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine()); 
+            int triple;
+            Calculator.Triple(a, out triple);
+            //a=variavel de origem / out triple onde guardei o resultado
+            Console.WriteLine(triple);
+            //imprimi a variavel onde guardei o resultado
+        }
+    }
+}
