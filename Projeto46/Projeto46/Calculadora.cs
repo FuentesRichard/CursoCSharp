de31efccce3d0b29370cea12659dfb23a5b7109d
FuentesRichard <richardfuentesrufino@gmail.com>
2021-06-09 17:09:24 -0300

    Projeto46

---
+++ Projeto46/Projeto46/Calculadora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Projeto46
+{
+    class Calculadora
+    {
+        public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+            for (int i =0;i<numbers.Length;i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ Projeto46/Projeto46/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Projeto46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int s = Calculadora.Sum(new int[] { 2, 4 }); Vira:
+            int s = Calculadora.Sum(2, 4);
+            Console.WriteLine("Soma:" + s);
+        }
+    }
+}
