ff47800b9802fc2b459fea1f0960eacee7b41eb8
FuentesRichard <richardfuentesrufino@gmail.com>
2021-06-09 18:59:32 -0300

    Projeto50/Foreach

---
+++ Projeto50/Projeto50/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Projeto50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //foreach - Sintaxe opccional para percorrer coleçoes:
+            //Para cada OBJETO 'obj' contido em VECT, FAÇA:
+            string[] vect = new string[] { "Maria", "Bob", "Alex" };
+            foreach(string obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
