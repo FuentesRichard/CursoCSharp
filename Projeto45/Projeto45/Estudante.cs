5af741eed5c8e10fbba8cd54d6c45afd90b46f9c
FuentesRichard <richardfuentesrufino@gmail.com>
2021-06-09 13:11:36 -0300

    Projeto45

---
+++ Projeto45/Projeto45/Estudante.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto45
+{
+    class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            nome = Nome;
+            email = Email;
+        }
+        public Estudante()
+        {
+
+        }
+        public override string ToString()
+        {
+            return ":" 
+                +Nome
+                + ", "
+                + Email;
+        }
+    }
+}
