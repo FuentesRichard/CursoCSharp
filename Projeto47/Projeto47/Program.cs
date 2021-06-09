55f5ffba2c7b9716bb0b8effc4c661281a648822
FuentesRichard <richardfuentesrufino@gmail.com>
2021-06-09 18:03:10 -0300

    Projeto47/ModificadorDeParametroRef

---
+++ Projeto47/Projeto47/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Projeto47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            //Triplo.Triplicar(x); - N funciona pq usa escopo local
+            Triplo.Triplicar(ref x);
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ Projeto47/Projeto47/Triplo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Projeto47
+{
+    class Triplo
+    {
+        //public static void Triplicar(int x) - Nao funciona s/ref pq usa escopo local
+        public static void Triplicar(ref int x)
+        {
+            x = x * 3;
+        }
+    }
+}
