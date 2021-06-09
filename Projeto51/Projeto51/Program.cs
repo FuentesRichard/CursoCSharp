51691d6d773ac349a271c823100d3bd8166f1ba1
FuentesRichard <richardfuentesrufino@gmail.com>
2021-06-09 19:15:18 -0300

    Projeto51/ListaTeoria

---
+++ Projeto51/Projeto51/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic; //namespace
+
+namespace Projeto51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Listas:
+            //Homogenea/Ordenada/IniciaVazia/1Elemento = 1Nodo
+            //Classe: List Namespace: System.Collections.Generic
+
+            List<string> list = new List<string>();//lista vazia
+
+            List<string> list2 = new List<string>() { "Maria" };
+
+            //Vantagens: 
+            //TamahoVariavel/FacilInserçaoEDeleçao("Pontos fortes da Lista sao os Fracos do Vetor e viceversa")
+            //Desvantagem:
+            //AcessoSequencialAosElementos
+
+        }
+    }
+}
