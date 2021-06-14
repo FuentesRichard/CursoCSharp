36fd217cd812d732ca40549cf1ef35a645091b3d
FuentesRichard <richardfuentesrufino@gmail.com>
2021-06-14 19:47:53 -0300

    Proejeto54/MatrizTeoria

---
+++ Projeto54/Projeto54/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Projeto54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Homogenea/Ordenada/AlocadaDe1SoVez
+            //Vantagem:AcessoImediatoPelasPosicoes
+            //Desvantagem:TamanhoFixo/DificilInsercaoDelecao
+
+            int[,] mat = new int[2, 3];
+            Console.WriteLine(mat.Length);//NDeElementos
+            Console.WriteLine(mat.Rank);//QuantidadeLinhas
+            Console.WriteLine(mat.GetLength(0));//QuantidadeNaDimensao'0'=>2Linhas
+            Console.WriteLine(mat.GetLength(1));//QuantidadeNaDomensao'1'=>3Colunas
+
+            
+        }
+    }
+}
