46933aff402312e9f2af3d0a211b8aaaf899848a
FuentesRichard <richardfuentesrufino@gmail.com>
2021-06-14 18:42:10 -0300

    Projeto53/ExercicioList

---
+++ Projeto53/Projeto53/Funcionario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Projeto53
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+
+        public Funcionario()
+        {
+        }
+
+        public Funcionario(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+   
+        public void SalaryIncrease(double aumento)
+        {
+            Salary += (aumento / 100.00 * Salary);
+        }
+        public override string ToString()
+        {
+            return Id
+         + ", "
+         + Name
+         + ", "
+         + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Projeto53/Projeto53/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Projeto53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many employees will be registered? ");
+            int N = int.Parse(Console.ReadLine());
+
+            List<Funcionario> list = new();
+
+            for (int i = 0; i <= N; i++)
+            {
+                Console.WriteLine("Emplyoee #" + i + ":");
+                Console.Write("Id:");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name:");
+                string name = Console.ReadLine();
+                Console.Write("Salary:");
+                double salary = double.Parse(Console.ReadLine());
+                list.Add(new Funcionario(id, name, salary));
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            
+            Funcionario funcionario = list.Find(x => x.Id == searchId);
+            if (funcionario != null)
+            {
+                Console.Write("Enter de percentage: ");
+                double percentage = double.Parse(Console.ReadLine());
+                funcionario.SalaryIncrease(percentage);
+
+            }            
+        }
+    }
+}
