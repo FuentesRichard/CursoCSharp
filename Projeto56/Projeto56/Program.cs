29682c529e55dd7a18762190526d0eca2302221e
FuentesRichard <richardfuentesrufino@gmail.com>
2021-06-21 15:13:57 -0300

    Projeto56/FuncoesString

---
+++ Projeto56/Projeto56/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Projeto56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Formatar:
+             * ToLower()=>TudoMinusculo
+             * ToUpper()=>TudoMinusculo
+             * Trim()=>ApagaOsEspaçosAntesEDepoisDoString
+             * IndexOf()=>ProcuraPrimeiraAparenciaDentroDoString(RetornaAPosicao)
+             * LastIndexOf()=>ProcuraUltimaAparenciaDentroDoString
+             * Substring(n)=>CortaAPartirDaPosiçao'n'
+             * Substring(n, m)=>CortaAPartirDaPosiçao'n'EApenas'm'Caracteres
+             * Replace('a', 'b')=>TrocaTodoCarectere'a'Por'b'(PossoTrocarMaisDe'1'Carectere)
+             * bool b1 = String.IsNullOrEmpty(string)=>TesteParaVerSeEEspaçoVazioOuNulo
+             * bool b2 = String.IsNullOrWhiteSpace(string)=>CasoEscrevaVariosEspaçosBrancos
+             * 
+             
+        }
+    }
+}
