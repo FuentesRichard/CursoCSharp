cd467f76fbf8c59af8d568c6cf2a5e42a427a893
FuentesRichard <richardfuentesrufino@gmail.com>
2021-06-23 20:18:26 -0300

    Projeto58/EnumEComposicao

---
+++ Projeto58/Projeto58/Entities/Departament.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Projeto58.Entities
+{
+    class Departament
+    {
+        public string Name { get; set; }
+
+        public Departament()
+        {
+
+        }
+
+        public Departament(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Projeto58/Projeto58/Entities/Enum/WorkerLevel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace Projeto58.Entities.Enum
+{
+    enum WorkerLevel : int
+    {
+        Junior,
+        MidLevel,
+        Senior
+    }
+}
