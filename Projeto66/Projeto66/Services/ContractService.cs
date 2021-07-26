837f927f39cc7eb90973d7aca57f24e06b26e173
FuentesRichard <richardfuentesrufino@gmail.com>
2021-07-26 16:59:08 -0300

    Projeto66/ExercicioComInterface

---
+++ Projeto66/Projeto66/Services/ContractService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Projeto66.Entities;
+
+namespace Projeto66.Services
+{
+    class ContractService
+    {
+        private IOnlinePaymentService _onlinePaymentService;
+        private PaypalService _paypalService;
+
+        public ContractService(IOnlinePaymentService onlinePaymentService)
+        {
+            _onlinePaymentService = onlinePaymentService;
+        }
+
+        public ContractService(PaypalService paypalService)
+        {
+            _paypalService = paypalService;
+        }
+
+        public void ProcessContract(Contract contract, int months)
+        {
+            double basicQuota = contract.TotalValue / months;
+            for (int i = 1; i <= months; i++)
+            {
+                DateTime date = contract.Date.AddMonths(i);
+                double updatedQuota = basicQuota + _onlinePaymentService.Interest(basicQuota, i);
+                double fullQuota = updatedQuota + _onlinePaymentService.PaymentFee(updatedQuota);
+                contract.AddInstallment(new Installment(date, fullQuota));
+            }
+        }
+    }
+}
---
+++ Projeto66/Projeto66/Services/IOnlinePaymentService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Projeto66.Services
+{
+    interface IOnlinePaymentService
+    {
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+    }
+}
